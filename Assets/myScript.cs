739309b4babe359a1ed7faf286f36d2d739fd5fd
taylorriordan <riordantaylor17@gmail.com>
2017-02-10 13:27:45 -0800

    character

---
+++ Assets/myScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            clone.GetComponent<Rigidbody>().AddForce(clone.transform.forward * speed);
+        }
+    }
+}
